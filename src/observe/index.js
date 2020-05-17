abdd2952d44b9a6a7bb849262cfa1410da47a238
fuheideMayuyu <xiaguangding@163.com>
2020-05-17 15:59:45 +0800

    feat: 对象的数据劫持

--- src/observe/index.js
+++ src/observe/index.js
@@ -1,4 +1,37 @@
 // object.definedProperty重新定义es5
+import {isObject} from '../utils/index.js'
+
+class Observer{
+  constructor(value){
+    this.walk(value)
+  }
+  walk(data){
+    let keys = Object.keys(data);
+    keys.forEach(key => {
+      defineReactive(data, key, data[key]) // 定义响应式数据
+    })
+  }
+}
+
+// 监听数据变化，动态添加get set方法
+function defineReactive(data, key, value){
+  observe(value) // 递归实现深度检测
+  Object.defineProperty(data, key, {
+    get(){
+      return value
+    },
+    set(newValue){
+      if(newValue === value) return
+      observe(value) // 继续劫持用户设置的值， 该值可能是个对象
+      value = newValue
+    }
+  })
+}
+
 export function observe(data){
-  console.log(data,'15456463')
+  let isObj = isObject(data)
+  if(!isObj) {
+    return
+  }
+  return new Observer(data) // 如果是对象， 则观测数据
 }
